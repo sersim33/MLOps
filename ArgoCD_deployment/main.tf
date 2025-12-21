4293fde18080bb2e6c0ece26e3340f7b08aeddd1
sersim33 <sersim33@gmail.com>
2025-12-21 12:21:35 +0200

    all

--- ArgoCD_deployment/main.tf
+++ ArgoCD_deployment/main.tf
@@ -9,7 +9,7 @@ resource "kubernetes_namespace" "argocd" {
 # --- Helm Release ArgoCD ---
 resource "helm_release" "argo" {
   name       = "argocd"
-  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  namespace  = kubernetes_namespace.argocd.metadata[0].name  # використовуємо створений namespace
 
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
@@ -19,41 +19,4 @@ resource "helm_release" "argo" {
   replace       = true
 
   values = [file("${path.module}/values/argocd-values.yaml")]
-}
-
-# --- Root App (App of Apps) через Kubernetes Manifest ---
-# Він підхоплює усі додатки з Git (mlflow, minio, postgres, pushgateway)
-resource "kubernetes_manifest" "argocd_root_app" {
-  manifest = {
-    apiVersion = "argoproj.io/v1alpha1"
-    kind       = "Application"
-    metadata = {
-      name      = "mlops-root"
-      namespace = var.argocd_namespace
-    }
-    spec = {
-      project = "default"
-      source = {
-        repoURL = var.app_repo_url
-        targetRevision = var.app_repo_branch
-        path = "argocd/applications"
-        directory = {
-          recurse = true
-        }
-      }
-      destination = {
-        server    = "https://kubernetes.default.svc"
-        namespace = var.argocd_namespace
-      }
-      syncPolicy = {
-        automated = {
-          prune    = true
-          selfHeal = true
-        }
-        syncOptions = ["CreateNamespace=true"]
-      }
-    }
-  }
-
-  depends_on = [helm_release.argo]
 }
